12c85e1aedcf52903842ea214c0cfece2884ae09
ASAP Marius <marius.poirierpro@gmail.com>
2025-12-02 14:30:38 +0100

    Implement user management features with PostgreSQL integration

---
+++ ryms/src/main/java/ry/ms/DAO/UserDAO.java
@@ -0,0 +1,16 @@
+package ry.ms.DAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import ry.ms.MODELS.User;
+
+public abstract class UserDAO {
+    
+    protected final Connection conn;
+
+    public UserDAO(Connection conn) {
+        this.conn = conn;
+    }
+    
+    public abstract User getUserById(String email) throws SQLException;
+}
